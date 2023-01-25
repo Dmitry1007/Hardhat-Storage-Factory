e2acf05c2d8bab659c759ecf0bd4df1763915bf1
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-25 13:04:33 -0700

    fix typo, add dotenv to manage private stuffs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  // defaultNetwork: "hardhat" // comes with rpc and private key
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.17",
 };
