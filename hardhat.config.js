4b1d009827b93d555296c4ff6215299188ec46d6
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-28 13:29:32 -0700

    Create gas report after running test

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -24,5 +25,12 @@ module.exports = {
   solidity: "0.8.17",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY
   },
 };
