4e5c8368f871f22607a0581ec48074203dc9a177
Dmitry Sky <dmitry.sky@icloud.com>
2023-01-28 13:38:55 -0700

    Add solidity coverage monitor

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("./tasks/block-number");
 require("hardhat-gas-reporter");
+require("solidity-coverage");
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
